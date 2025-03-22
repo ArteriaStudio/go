43d2c7aa9639af324acb92d9f94900aa1152371b
Rink <rink@mxd.mesh.ne.jp>
2025-03-22 16:06:13 +0900

    add gemini go

---
+++ gemini/main.go
@@ -0,0 +1,42 @@
+﻿// 　Gemini クライアント
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/google/generative-ai-go/genai"
+	"google.golang.org/api/option"
+)
+
+func printResponse(resp *genai.GenerateContentResponse) {
+	for _, cand := range resp.Candidates {
+		if cand.Content != nil {
+			for _, part := range cand.Content.Parts {
+				fmt.Println(part)
+			}
+		}
+	}
+	fmt.Println("---")
+}
+
+func main() {
+	ctx := context.Background()
+	// Access your API key as an environment variable (see "Set up your API key" above)
+	client, err := genai.NewClient(ctx, option.WithAPIKey(os.Getenv("API_KEY")))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Close()
+
+	model := client.GenerativeModel("gemini-1.5-flash")
+	resp, err := model.GenerateContent(ctx, genai.Text("日本語は使えますか？"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	printResponse(resp)
+
+	//resp = client.ListModels(context)
+}
