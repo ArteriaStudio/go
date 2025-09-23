c09a7bb52b9255697d5e623c2fc31eb117bae3c9
Rink <rink@mxd.mesh.ne.jp>
2025-09-23 09:39:05 +0900

    update

--- crossAdaptor/graphtutorial.go
+++ crossAdaptor/graphtutorial.go
@@ -1,173 +1,247 @@
 package main
 
 import (
-    "fmt"
-    "crossAdaptor/graphhelper"
-    "log"
-    "time"
+	"context"
+	"crossAdaptor/graphhelper"
+	"fmt"
+	"log"
+	"time"
 
-    "github.com/joho/godotenv"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/joho/godotenv"
+	msgraphsdk "github.com/microsoftgraph/msgraph-sdk-go"
+	"github.com/microsoftgraph/msgraph-sdk-go/users"
 )
 
+// 　サービス、バックエンド型アプリケーション向けの認証フロー（クライアント資格フロー）
+// 　https://learn.microsoft.com/ja-jp/graph/sdks/choose-authentication-providers?tabs=go#client-credentials-provider
+// 　代理フロー
+// 　https://learn.microsoft.com/ja-jp/entra/identity-platform/v2-oauth2-on-behalf-of-flow
+func main3() {
+	// 1. 環境変数を設定するか、直接コードに埋め込む
+	//　本番環境では環境変数やAzure Key Vaultなどを使用することを推奨します
+	tenantID := "11838164-2c3e-49f4-845e-f93e948ac7c3"
+	clientID := "4aeb6947-0160-4eaa-a1fa-021392e434e9"
+	clientSecret := ""
+
+	// 2. クライアントクレデンシャルグラントフローのための認証情報を作成
+	cred, err := azidentity.NewClientSecretCredential(tenantID, clientID, clientSecret, nil)
+	if err != nil {
+		fmt.Printf("認証情報の作成に失敗しました: %v\n", err)
+		return
+	}
+
+	// 3. Graph APIクライアントの作成
+	graphClient, err := msgraphsdk.NewGraphServiceClientWithCredentials(cred, []string{"https://graph.microsoft.com/.default"})
+	if err != nil {
+		fmt.Printf("Graph クライアントの作成に失敗しました: %v\n", err)
+		return
+	}
+
+	query := users.UserItemRequestBuilderGetQueryParameters{
+		// Only request specific properties
+		Select: []string{"displayName", "mail", "userPrincipalName"},
+	}
+
+	pUser, err := graphClient.Me().Get(context.Background(),
+		&users.UserItemRequestBuilderGetRequestConfiguration{
+			QueryParameters: &query,
+		})
+	if err != nil {
+		fmt.Printf("Graph ユーザー情報の取得に失敗しました: %v\n", err)
+		return
+	}
+
+	fmt.Printf("Hello, %s!\n", *pUser.GetDisplayName())
+
+	// For Work/school accounts, email is in Mail property
+	// Personal accounts, email is in UserPrincipalName
+	email := pUser.GetMail()
+	if email == nil {
+		email = pUser.GetUserPrincipalName()
+	}
+
+	fmt.Printf("Email: %s\n", *email)
+	fmt.Println()
+
+	/*
+		// 4. Graph APIへのリクエスト (例: 全ユーザーの取得)
+		// .Top(10)は取得件数を10件に制限
+		result, err := graphClient.Users().Get(context.Background(), &users.UsersRequestBuilderGetRequestConfiguration{
+			QueryParameters: &users.UsersRequestBuilderGetQueryParameters{},
+		})
+		if err != nil {
+			fmt.Printf("ユーザー情報の取得に失敗しました: %v\n", err)
+			return
+		}
+
+		// 5. 結果の表示
+		for _, user := range result.GetValue() {
+			fmt.Printf("ユーザー名: %s, メールアドレス: %s\n", *user.GetDisplayName(), *user.GetMail())
+		}
+	*/
+
+}
+
 func main() {
-    fmt.Println("Go Graph Tutorial")
-    fmt.Println()
+	fmt.Println("Go Graph Tutorial")
+	fmt.Println()
 
-    // Load .env files
-    // .env.local takes precedence (if present)
-    godotenv.Load(".env.local")
-    err := godotenv.Load()
-    if err != nil {
-        log.Fatal("Error loading .env")
-    }
+	// Load .env files
+	// .env.local takes precedence (if present)
+	godotenv.Load(".env.local")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env")
+	}
 
-    graphHelper := graphhelper.NewGraphHelper()
+	graphHelper := graphhelper.NewGraphHelper()
 
-    initializeGraph(graphHelper)
+	initializeGraph(graphHelper)
 
-    greetUser(graphHelper)
+	greetUser(graphHelper)
 
-    var choice int64 = -1
+	var choice int64 = -1
 
-    for {
-        fmt.Println("Please choose one of the following options:")
-        fmt.Println("0. Exit")
-        fmt.Println("1. Display access token")
-        fmt.Println("2. List my inbox")
-        fmt.Println("3. Send mail")
-        fmt.Println("4. Make a Graph call")
+	for {
+		fmt.Println("Please choose one of the following options:")
+		fmt.Println("0. Exit")
+		fmt.Println("1. Display access token")
+		fmt.Println("2. List my inbox")
+		fmt.Println("3. Send mail")
+		fmt.Println("4. Make a Graph call")
 
-        _, err = fmt.Scanf("%d", &choice)
-        if err != nil {
-            choice = -1
-        }
+		_, err = fmt.Scanf("%d", &choice)
+		if err != nil {
+			choice = -1
+		}
 
-        switch choice {
-        case 0:
-            // Exit the program
-            fmt.Println("Goodbye...")
-        case 1:
-            // Display access token
-            displayAccessToken(graphHelper)
-        case 2:
-            // List emails from user's inbox
-            listInbox(graphHelper)
-        case 3:
-            // Send an email message
-            sendMail(graphHelper)
-        case 4:
-            // Run any Graph code
-            makeGraphCall(graphHelper)
-        default:
-            fmt.Println("Invalid choice! Please try again.")
-        }
+		switch choice {
+		case 0:
+			// Exit the program
+			fmt.Println("Goodbye...")
+		case 1:
+			// Display access token
+			displayAccessToken(graphHelper)
+		case 2:
+			// List emails from user's inbox
+			listInbox(graphHelper)
+		case 3:
+			// Send an email message
+			sendMail(graphHelper)
+		case 4:
+			// Run any Graph code
+			makeGraphCall(graphHelper)
+		default:
+			fmt.Println("Invalid choice! Please try again.")
+		}
 
-        if choice == 0 {
-            break
-        }
-    }
+		if choice == 0 {
+			break
+		}
+	}
 }
 
 func initializeGraph(graphHelper *graphhelper.GraphHelper) {
-    err := graphHelper.InitializeGraphForUserAuth()
-    if err != nil {
-        log.Panicf("Error initializing Graph for user auth: %v\n", err)
-    }
+	err := graphHelper.InitializeGraphForUserAuth()
+	if err != nil {
+		log.Panicf("Error initializing Graph for user auth: %v\n", err)
+	}
 }
 
 func greetUser(graphHelper *graphhelper.GraphHelper) {
-    user, err := graphHelper.GetUser()
-    if err != nil {
-        log.Panicf("Error getting user: %v\n", err)
-    }
+	user, err := graphHelper.GetUser()
+	if err != nil {
+		log.Panicf("Error getting user: %v\n", err)
+	}
 
-    fmt.Printf("Hello, %s!\n", *user.GetDisplayName())
+	fmt.Printf("Hello, %s!\n", *user.GetDisplayName())
 
-    // For Work/school accounts, email is in Mail property
-    // Personal accounts, email is in UserPrincipalName
-    email := user.GetMail()
-    if email == nil {
-        email = user.GetUserPrincipalName()
-    }
+	// For Work/school accounts, email is in Mail property
+	// Personal accounts, email is in UserPrincipalName
+	email := user.GetMail()
+	if email == nil {
+		email = user.GetUserPrincipalName()
+	}
 
-    fmt.Printf("Email: %s\n", *email)
-    fmt.Println()
+	fmt.Printf("Email: %s\n", *email)
+	fmt.Println()
 }
 
 func displayAccessToken(graphHelper *graphhelper.GraphHelper) {
-    token, err := graphHelper.GetUserToken()
-    if err != nil {
-        log.Panicf("Error getting user token: %v\n", err)
-    }
+	token, err := graphHelper.GetUserToken()
+	if err != nil {
+		log.Panicf("Error getting user token: %v\n", err)
+	}
 
-    fmt.Printf("User token: %s", *token)
-    fmt.Println()
+	fmt.Printf("User token: %s", *token)
+	fmt.Println()
 }
 
 func listInbox(graphHelper *graphhelper.GraphHelper) {
-    messages, err := graphHelper.GetInbox()
-    if err != nil {
-        log.Panicf("Error getting user's inbox: %v", err)
-    }
+	messages, err := graphHelper.GetInbox()
+	if err != nil {
+		log.Panicf("Error getting user's inbox: %v", err)
+	}
 
-    // Load local time zone
-    // Dates returned by Graph are in UTC, use this
-    // to convert to local
-    location, err := time.LoadLocation("Local")
-    if err != nil {
-        log.Panicf("Error getting local timezone: %v", err)
-    }
+	// Load local time zone
+	// Dates returned by Graph are in UTC, use this
+	// to convert to local
+	location, err := time.LoadLocation("Local")
+	if err != nil {
+		log.Panicf("Error getting local timezone: %v", err)
+	}
 
-    // Output each message's details
-    for _, message := range messages.GetValue() {
-        fmt.Printf("Message: %s\n", *message.GetSubject())
-        fmt.Printf("  From: %s\n", *message.GetFrom().GetEmailAddress().GetName())
+	// Output each message's details
+	for _, message := range messages.GetValue() {
+		fmt.Printf("Message: %s\n", *message.GetSubject())
+		fmt.Printf("  From: %s\n", *message.GetFrom().GetEmailAddress().GetName())
 
-        status := "Unknown"
-        if *message.GetIsRead() {
-            status = "Read"
-        } else {
-            status = "Unread"
-        }
-        fmt.Printf("  Status: %s\n", status)
-        fmt.Printf("  Received: %s\n", (*message.GetReceivedDateTime()).In(location))
-    }
+		status := "Unknown"
+		if *message.GetIsRead() {
+			status = "Read"
+		} else {
+			status = "Unread"
+		}
+		fmt.Printf("  Status: %s\n", status)
+		fmt.Printf("  Received: %s\n", (*message.GetReceivedDateTime()).In(location))
+	}
 
-    // If GetOdataNextLink does not return nil,
-    // there are more messages available on the server
-    nextLink := messages.GetOdataNextLink()
+	// If GetOdataNextLink does not return nil,
+	// there are more messages available on the server
+	nextLink := messages.GetOdataNextLink()
 
-    fmt.Println()
-    fmt.Printf("More messages available? %t\n", nextLink != nil)
-    fmt.Println()
+	fmt.Println()
+	fmt.Printf("More messages available? %t\n", nextLink != nil)
+	fmt.Println()
 }
 
 func sendMail(graphHelper *graphhelper.GraphHelper) {
-    // Send mail to the signed-in user
-    // Get the user for their email address
-    user, err := graphHelper.GetUser()
-    if err != nil {
-        log.Panicf("Error getting user: %v", err)
-    }
+	// Send mail to the signed-in user
+	// Get the user for their email address
+	user, err := graphHelper.GetUser()
+	if err != nil {
+		log.Panicf("Error getting user: %v", err)
+	}
 
-    // For Work/school accounts, email is in Mail property
-    // Personal accounts, email is in UserPrincipalName
-    email := user.GetMail()
-    if email == nil {
-        email = user.GetUserPrincipalName()
-    }
+	// For Work/school accounts, email is in Mail property
+	// Personal accounts, email is in UserPrincipalName
+	email := user.GetMail()
+	if email == nil {
+		email = user.GetUserPrincipalName()
+	}
 
-    subject := "Testing Microsoft Graph"
-    body := "Hello world!"
-    err = graphHelper.SendMail(&subject, &body, email)
-    if err != nil {
-        log.Panicf("Error sending mail: %v", err)
-    }
+	subject := "Testing Microsoft Graph"
+	body := "Hello world!"
+	err = graphHelper.SendMail(&subject, &body, email)
+	if err != nil {
+		log.Panicf("Error sending mail: %v", err)
+	}
 
-    fmt.Println("Mail sent.")
-    fmt.Println()
+	fmt.Println("Mail sent.")
+	fmt.Println()
 }
 
 func makeGraphCall(graphHelper *graphhelper.GraphHelper) {
-    // TODO
+	// TODO
 }
